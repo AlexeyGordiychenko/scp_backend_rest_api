0bc7c78cd840e47e8de830c0f5f1ac5d79e43b02
elidacon <elidacon@student.21-school.ru>
2024-05-07 00:42:25 +0500

    Add 'Client' and 'Address' models

---
+++ src/app/models/address.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from sqlmodel import SQLModel
+
+from app.models.base import IdMixin
+
+
+class AddressBase(SQLModel):
+    country: str
+    city: str
+    street: str
+
+
+class AddressCreate(AddressBase):
+    pass
+
+
+class AddressUpdate(AddressBase):
+    country: Optional[str] = None
+    city: Optional[str] = None
+    street: Optional[str] = None
+
+
+class Address(IdMixin, AddressBase, table=True):
+    __tablename__ = "address"
+
+
+class AddressResponse(Address, table=False):
+    pass
---
+++ src/app/models/client.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel, SQLType
+from datetime import date, datetime
+from uuid import UUID
+
+from app.models.base import IdMixin
+
+
+class ClientBase(SQLModel):
+    client_name: str = Field(nullable=False)
+    client_surname: str = Field(nullable=False)
+    birthday: date = Field(nullable=False)
+    gender: str = Field(
+        nullable=False,
+        regex="^(M|F)$",
+        constraints=[SQLType("CHAR(1) CHECK (gender IN ('M', 'F'))")],
+    )
+    registration_date: datetime = Field(nullable=False)
+    address_id: UUID = Field(nullable=False, foreign_key="address.id")
+
+
+class ClientCreate(ClientBase):
+    pass
+
+
+class ClientUpdate(ClientBase):
+    client_name: Optional[str] = None
+    client_surname: Optional[str] = None
+    birthday: Optional[date] = None
+    gender: Optional[str] = None
+    registration_date: Optional[datetime] = None
+    address_id: Optional[UUID] = None
+
+
+class Client(IdMixin, ClientBase, table=True):
+    __tablename__ = "client"
+
+
+class ClientResponse(Client, table=False):
+    pass
