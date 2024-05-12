8d3de400406451e0f92e2678ecbbb4ad4d94f807
elidacon <elidacon@student.21-school.ru>
2024-05-12 23:27:58 +0500

    Transactional: Add 'refresh' flag

--- src/shopAPI/core/database/transactional.py
+++ src/shopAPI/core/database/transactional.py
@@ -4,12 +4,17 @@ from shopAPI.core.database import session
 
 
 class Transactional:
+    def __init__(self, refresh: bool = False):
+        self.refresh = refresh
+
     def __call__(self, function):
         @wraps(function)
         async def decorator(*args, **kwargs):
             try:
                 result = await function(*args, **kwargs)
                 await session.commit()
+                if self.refresh:
+                    await session.refresh(result)
                 return result
             except Exception as exception:
                 await session.rollback()
