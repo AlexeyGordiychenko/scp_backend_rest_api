df10b34943c0181eb402057c4a1d28b6ba258858
elidacon <elidacon@student.21-school.ru>
2024-05-13 19:32:06 +0500

    Client: Add address_id, change the alembic initial version

--- src/shopAPI/app/models/client.py
+++ src/shopAPI/app/models/client.py
@@ -1,6 +1,11 @@
+from uuid import UUID
+
+from sqlmodel import Field
 from shopAPI.core.database.mixins.id import IdMixin
 from shopAPI.core.schemas.base import ClientBase
 
 
 class Client(IdMixin, ClientBase, table=True):
     __tablename__ = "client"
+
+    address_id: UUID | None = Field(default=None, foreign_key="address.id")
