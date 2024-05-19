2975cad4b8baf6f198e69b1ae8febf61abe89348
elidacon <elidacon@student.21-school.ru>
2024-05-19 21:31:55 +0500

    Simplify db session handling
    - remove 'reader' and 'writer' engines
    - remove context (use asyncio.current_task instead)
    - remove sql alchemy middleware

--- src/shopAPI/database.py
+++ src/shopAPI/database.py
@@ -1,6 +1,5 @@
-from contextvars import ContextVar, Token
+from asyncio import current_task
 from functools import wraps
-from typing import Union
 from uuid import UUID
 from uuid_extensions import uuid7
 
@@ -9,8 +8,7 @@ from sqlalchemy.ext.asyncio import (
     async_scoped_session,
     create_async_engine,
 )
-from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy.sql.expression import Delete, Insert, Update
+from sqlalchemy.orm import sessionmaker
 from sqlmodel import Field, SQLModel
 
 from shopAPI.config import settings
@@ -45,52 +43,22 @@ class Transactional:
         return decorator
 
 
-session_context: ContextVar[str] = ContextVar("session_context")
-
-
-def get_session_context() -> str:
-    return session_context.get()
-
-
-def set_session_context(session_id: str) -> Token:
-    return session_context.set(session_id)
-
-
-def reset_session_context(context: Token) -> None:
-    session_context.reset(context)
-
-
-def get_engine():
-    return create_async_engine(
-        str(settings.POSTGRES_URI),
-        echo=settings.POSTGRES_ECHO,
-        future=True,
-        pool_size=settings.POSTGRES_POOL_SIZE,
-    )
-
-
-engines = {
-    "writer": get_engine(),
-    "reader": get_engine(),
-}
-
-
-class RoutingSession(Session):
-    def get_bind(self, mapper=None, clause=None, **kwargs):
-        if self._flushing or isinstance(clause, (Update, Delete, Insert)):
-            return engines["writer"].sync_engine
-        return engines["reader"].sync_engine
-
+engine = create_async_engine(
+    str(settings.POSTGRES_URI),
+    echo=settings.POSTGRES_ECHO,
+    future=True,
+    pool_size=settings.POSTGRES_POOL_SIZE,
+)
 
 async_session_factory = sessionmaker(
+    bind=engine,
     class_=AsyncSession,
-    sync_session_class=RoutingSession,
     expire_on_commit=False,
 )
 
-session: Union[AsyncSession, async_scoped_session] = async_scoped_session(
+session = async_scoped_session(
     session_factory=async_session_factory,
-    scopefunc=get_session_context,
+    scopefunc=current_task,
 )
 
 
--- src/shopAPI/server.py
+++ src/shopAPI/server.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware import Middleware
 
 from shopAPI.routers import api_router, status_router
 from shopAPI.config import settings
-from shopAPI.middlewares import SQLAlchemyMiddleware
 
 
 def get_application():
@@ -11,7 +9,6 @@ def get_application():
         title=settings.PROJECT_NAME,
         version=settings.VERSION,
         docs_url="/swagger",
-        middleware=[Middleware(SQLAlchemyMiddleware)],
     )
     app.include_router(api_router, prefix="/api")
     app.include_router(status_router)
