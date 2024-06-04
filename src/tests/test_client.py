995e79fef64537faba12661f92459082531bb00e
elidacon <elidacon@student.21-school.ru>
2024-06-04 20:58:14 +0500

    Test: Add test_get_client func

--- src/tests/test_client.py
+++ src/tests/test_client.py
@@ -32,3 +32,18 @@ async def test_post_client(
         ClientResponseWithAddress.model_validate(client_in_db).model_dump(mode="json")
         == client_payload
     )
+
+
+@pytest.mark.asyncio
+async def test_get_client(client: AsyncClient, client_payload: dict) -> None:
+    response_create = await client.post(
+        "client",
+        json=client_payload,
+    )
+    assert response_create.status_code == 201
+    response_create_json = response_create.json()
+    assert "id" in response_create_json
+
+    response_get = await client.get(f"client/{response_create_json['id']}")
+    assert response_get.status_code == 200
+    assert response_get.json() == response_create_json
