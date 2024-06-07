badafde5bf2b833b6ddbd5aa11a0118064b9dcff
elidacon <elidacon@student.21-school.ru>
2024-06-07 13:10:53 +0500

    Test: Add test_get_all_by_name_double func

--- src/tests/test_client.py
+++ src/tests/test_client.py
@@ -93,3 +93,22 @@ async def test_get_all_by_name(
     response_get_json = response_get.json()
     assert len(response_get_json) == 1
     assert response_get_json[0] == created_client
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("client_payloads", [2], indirect=True)
+async def test_get_all_by_name_double(
+    client: AsyncClient,
+    create_clients: Callable[[dict], Awaitable[dict]],
+    client_payloads: List[dict],
+) -> None:
+    client_payloads.append(dict(client_payloads[1]))
+    await create_clients(client_payloads)
+    created_client = client_payloads[1]
+
+    response_get = await client.get(f"client/all?name={created_client['client_name']}")
+    assert response_get.status_code == 200
+    response_get_json = response_get.json()
+    assert len(response_get_json) == 2
+    for i, client_payload in enumerate(client_payloads[1:]):
+        assert client_payload == response_get_json[i]
