a724ea5d030faea037506240abbce1359b5822b8
elidacon <elidacon@student.21-school.ru>
2024-06-15 20:38:14 +0500

    Test: Add test for incorrect client birthday

--- src/tests/test_client_error.py
+++ src/tests/test_client_error.py
@@ -18,3 +18,18 @@ async def test_post_client_gender(
         json=client_payload,
     )
     await utils.check_422_error(response_create, "gender")
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("client_payloads", [1], indirect=True)
+@pytest.mark.parametrize("invalid_birthday", ("1", "1-1-2024 11:11:11"))
+async def test_post_client_birthday(
+    client: AsyncClient, client_payloads: List[dict], invalid_birthday: str
+) -> None:
+    client_payload = client_payloads[0]
+    client_payload["birthday"] = invalid_birthday
+    response_create = await client.post(
+        "client",
+        json=client_payload,
+    )
+    await utils.check_422_error(response_create, "birthday")
